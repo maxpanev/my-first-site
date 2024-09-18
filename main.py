577013fe9c304c8d23882f844a5bf029f2f56147
maxpanev <maxpanev170100@gmail.com>
2024-09-18 17:53:30 +0300

    add new code

--- main.py
+++ main.py
@@ -1,52 +1,60 @@
-import requests
-from bs4 import BeautifulSoup
-from googletrans import Translator
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
-translator = Translator()
+def search_wikipedia(query):
+    search_box = browser.find_element(By.NAME, "search")
+    search_box.clear()
+    search_box.send_keys(query)
+    search_box.submit()
+    time.sleep(2)
 
-def get_english_words():
-    url = "https://randomword.com/"
-    try:
-        response = requests.get(url)
+def list_paragraphs():
+    paragraphs = browser.find_elements(By.CSS_SELECTOR, "p")
+    for i, paragraph in enumerate(paragraphs[:5], 1):
+        print(f"{i}. {paragraph.text[:100]}...")
 
-        soup = BeautifulSoup(response.content, "html.parser")
-        english_words = soup.find("div", id="random_word").text.strip()
-        word_definition = soup.find("div", id="random_word_definition").text.strip()
+def list_links():
+    links = browser.find_elements(By.CSS_SELECTOR, "a[href^='/wiki/']")
+    return links
 
-        # Переводим слово на русский
-        russian_word = translator.translate(english_words, src='en', dest='ru').text
-        russian_definition = translator.translate(word_definition, src='en', dest='ru').text
+def main():
+    browser.get("https://ru.wikipedia.org")
 
-        # Добавим перевод русских слов в словарь
-        return {
-            "english_words": english_words,
-            "word_definition": word_definition,
-            "russian_word": russian_word,
-            "russian_definition": russian_definition
-        }
-    except Exception as e:
-        print(f"Произошла ошибка: {e}")
+    query = input("Введите запрос: ")
+    search_wikipedia(query)
 
-def word_game():
-    print("Добро пожаловать в игру")
     while True:
-        word_dict = get_english_words()
+        print("\nВыберите действие:")
+        print("1. Листать параграфы текущей статьи")
+        print("2. Перейти на одну из связанных страниц")
+        print("3. Выйти из программы")
 
-        # Переводим слово на русский
-        russian_word = word_dict.get("russian_word")
-        russian_definition = word_dict.get("russian_definition")
+        choice = input("Ваш выбор: ")
 
-        print(f"Значение слова - {russian_definition}")
-        user = input("Что это за слово? ")
-        if user.lower() == russian_word.lower():
-            print("Все верно!")
-        else:
-            print(f"Ответ неверный, было загадано это слово - {russian_word}")
+        if choice == "1":
+            list_paragraphs()
+        elif choice == "2":
+            links = list_links()
+            for i, link in enumerate(links[:10], 1):  # Ограничимся первыми 10 ссылками для простоты
+                print(f"{i}. {link.text} ({link.get_attribute('href')})")
 
-        play_again = input("Хотите сыграть еще раз? y/n ")
-        if play_again.lower() != "y":
-            print("Спасибо за игру!")
+            link_choice = int(input("Введите номер ссылки для перехода: ")) - 1
+
+            if 0 <= link_choice < len(links):
+                browser.get(links[link_choice].get_attribute('href'))
+                time.sleep(2)
+            else:
+                print("Некорректный выбор.")
+        elif choice == "3":
+            print("Выход из программы.")
             break
-            
+        else:
+            print("Некорректный выбор.")
 
-word_game()
+if __name__ == "__main__":
+    browser = webdriver.Chrome()
+    try:
+        main()
+    finally:
+        browser.quit()
